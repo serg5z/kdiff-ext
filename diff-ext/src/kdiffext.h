b34d9adc3da2b0dce53b7c4d3226fbc57ca71a9e
serg_z <unknown>
2008-04-25 03:30:38 +0000

    work in progress -- move to cmake

--- diff-ext/src/kdiffext.h
+++ diff-ext/src/kdiffext.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 Sergey Zorin. All rights reserved.
+ * Copyright (c) 2007-2008 Sergey Zorin. All rights reserved.
  *
  * This software is distributable under the BSD license. See the terms
  * of the BSD license in the COPYING file provided with this software.
@@ -7,10 +7,6 @@
  */
 #ifndef __KDIFFEXT_H__
 #define __KDIFFEXT_H__
-
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
 
 #include <qstring.h>
 #include <qsignalmapper.h>
